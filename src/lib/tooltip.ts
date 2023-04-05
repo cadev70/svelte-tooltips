2d75dab95205ec3ea19ae054f2601877a5aa7ba1
carly <carly@weibyapps.com>
2023-04-05 13:39:13 +0800

    modify to use inline function

--- src/lib/tooltip.ts
+++ src/lib/tooltip.ts
@@ -4,25 +4,79 @@ import { createPopper, type Instance } from '@popperjs/core';
 
 const SHOW_EVENTS = ['mouseenter', 'focus'];
 const HIDE_EVENTS = ['mouseleave', 'blur'];
-let _content: string;
-let _disabled: boolean;
-let _offset: number;
-let _placement: Placement;
-let _popper: Instance | null;
-let _reference: HTMLElement;
-let _showArrow: boolean;
-let _tooltip: Tooltip;
-let _tooltipElement: HTMLElement;
 
 export function tooltip(reference: HTMLElement, options: TooltipOptions) {
-	_reference = reference;
+	let _content: string;
+	let _disabled: boolean;
+	let _offset: number;
+	let _placement: Placement;
+	let _popper: Instance | null;
+	let _showArrow: boolean;
+	let _tooltip: Tooltip;
+	let _tooltipElement: HTMLElement;
+
+	const updateOptions = (options: TooltipOptions) => {
+		const {
+			content = '',
+			disabled = false,
+			offset = 8,
+			placement = 'top',
+			showArrow = true
+		} = options;
+		_content = content;
+		_disabled = disabled;
+		_offset = offset;
+		_placement = placement;
+		_showArrow = showArrow;
+	};
+
+	const show = () => {
+		if (_disabled) {
+			return;
+		}
+		if (!reference.parentElement) {
+			console.warn('Missing parent element of reference element');
+			return;
+		}
+		_tooltip = new Tooltip({
+			target: reference.parentElement!,
+			props: { content: _content, showArrow: _showArrow }
+		});
+
+		if (!_tooltip.element) {
+			console.warn('Missing tooltip element.');
+			return;
+		}
+		_tooltipElement = _tooltip.element!;
+		_tooltipElement.setAttribute('data-show', '');
+
+		_popper = createPopper(reference, _tooltipElement, {
+			modifiers: [
+				{
+					name: 'offset',
+					options: {
+						offset: [0, _offset]
+					}
+				}
+			],
+			placement: _placement
+		});
+	};
+
+	const hide = () => {
+		_tooltipElement?.removeAttribute('data-show');
+		_popper?.destroy();
+		_popper = null;
+		_tooltip?.$destroy();
+	};
+
 	updateOptions(options);
 
 	SHOW_EVENTS.forEach((event) => {
-		_reference.addEventListener(event, show);
+		reference.addEventListener(event, show);
 	});
 	HIDE_EVENTS.forEach((event) => {
-		_reference.addEventListener(event, hide);
+		reference.addEventListener(event, hide);
 	});
 
 	return {
@@ -32,66 +86,11 @@ export function tooltip(reference: HTMLElement, options: TooltipOptions) {
 		destroy() {
 			_popper?.destroy();
 			SHOW_EVENTS.forEach((event) => {
-				_reference.removeEventListener(event, show);
+				reference.removeEventListener(event, show);
 			});
 			HIDE_EVENTS.forEach((event) => {
-				_reference.removeEventListener(event, hide);
+				reference.removeEventListener(event, hide);
 			});
 		}
 	};
-}
-
-function updateOptions(options: TooltipOptions) {
-	const {
-		content = '',
-		disabled = false,
-		offset = 8,
-		placement = 'top',
-		showArrow = true
-	} = options;
-	_content = content;
-	_disabled = disabled;
-	_offset = offset;
-	_placement = placement;
-	_showArrow = showArrow;
-}
-
-function show() {
-	if (_disabled) {
-		return;
-	}
-	if (!_reference.parentElement) {
-		console.warn('Missing parent element of reference element');
-		return;
-	}
-	_tooltip = new Tooltip({
-		target: _reference.parentElement!,
-		props: { content: _content, showArrow: _showArrow }
-	});
-
-	if (!_tooltip.element) {
-		console.warn('Missing tooltip element.');
-		return;
-	}
-	_tooltipElement = _tooltip.element!;
-	_tooltipElement.setAttribute('data-show', '');
-
-	_popper = createPopper(_reference, _tooltipElement, {
-		modifiers: [
-			{
-				name: 'offset',
-				options: {
-					offset: [0, _offset]
-				}
-			}
-		],
-		placement: _placement
-	});
-}
-
-function hide() {
-	_tooltipElement?.removeAttribute('data-show');
-	_popper?.destroy();
-	_popper = null;
-	_tooltip?.$destroy();
 }
